bb39805539fe4f35ea8fa6c4d4634b17dd62d4a4
Cancey Dejean <canceyd@gmail.com>
2024-03-23 15:32:53 -0500

    Add CreationBottomBar component and dynamic loading of Map component

--- app/create/[id]/address/page.tsx
+++ app/create/[id]/address/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import CreationBottomBar from "@/components/CreationBottomBar";
 import {
   Select,
   SelectContent,
@@ -8,12 +9,19 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Skeleton } from "@/components/ui/skeleton";
 import { useCountries } from "@/lib/getCountries";
+import dynamic from "next/dynamic";
 import { useState } from "react";
 
 export default function AddressRoute() {
   const { getAllCountries } = useCountries();
   const [locationValue, setLocationValue] = useState("");
+
+  const LazyMap = dynamic(() => import("@/components/Map"), {
+    ssr: false,
+    loading: () => <Skeleton className="h-[50vh] w-full" />,
+  });
   return (
     <>
       <div className="w-3/5 mx-auto">
@@ -27,7 +35,7 @@ export default function AddressRoute() {
         <input type="hidden" name="countryValue" value="" />
         <div className="w-3/5 mx-auto mb-36">
           <div className="mb-5">
-            <Select required>
+            <Select required onValueChange={(value) => setLocationValue(value)}>
               <SelectTrigger className="w-full">
                 <SelectValue placeholder="Select a Country" />
               </SelectTrigger>
@@ -44,10 +52,10 @@ export default function AddressRoute() {
             </Select>
           </div>
 
-          {/* <LazyMap locationValue={locationValue} /> */}
+          <LazyMap locationValue={locationValue} />
         </div>
 
-        {/* <CreatioBottomBar /> */}
+        <CreationBottomBar />
       </form>
     </>
   );
